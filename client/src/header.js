74aeda50fd530bd67f70d1c968e142a193c5e70a
Russ <yunsubin481@gmail.com>
2022-12-23 15:03:27 +0900

    header

---
+++ client/src/header.js
@@ -0,0 +1,89 @@
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+        <div class="nav">
+                    {/* <!--logo--> */}
+                <span class="nav__CI">
+                    <a href="/"><img class="nav__logo" src ="https://opensea.io/static/images/logos/opensea.svg"/></a>
+                    <a class="nav__siteName" href="/">OpenSea</a> 
+                </span>
+
+                    {/* <!--Search items, collections, and accounts--> */}
+                <span class="nav__search">
+                    {/* <input class="nav__searchbar" aria-label="Search OpenSea" aria-multiline="false" placeholder="    Search items, collections, and accounts" role="searchbox" type="search" value style = "cursor: text;"></input> */}
+                </span>
+                <ul>
+                    <div class="nav__menu">
+                        {/* <!--Explore--> */}
+                        <ul><a href="">Explore</a>
+                            {/* <!-- <li>All NFTs</li>
+                            <li>Art</li>
+                            <li>Collectibles</li>
+                            <li>Domain Names</li>
+                            <li>Music</li>
+                            <li>Photography</li>
+                            <li>Sports</li>
+                            <li>Trading Cards</li>
+                            <li>Utility</li>
+                            <li>Virtual Worlds</li> --> */}
+                        </ul>
+                        {/* <!--Stats--> */}
+                        <ul><a href="">Stats</a>
+                            {/* <!-- <li>Ranking</li>
+                            <li>Activity</li> --> */}
+                        </ul>
+                        {/* <!--Resources--> */}
+                        <ul><a href="">Resources</a>
+                            {/* <!-- <li>Learn</li>
+                            <li>Help</li>
+                            <li>Platform Status</li>
+                            <li>Partners</li>
+                            <li>Taxes</li>
+                            <li>Blogs</li>
+                            <li>Docs</li>
+                            <li>Newsletter</li> --> */}
+                            {/* <!--Community.symbols--> */}
+                            {/* <!-- <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                            <i class="fa-brands fa-discord"></i>
+                            <i class="fa-brands fa-reddit-alien"></i>
+                            <i class="fa-brands fa-youtube"></i>
+                            <i class="fa-brands fa-tiktok"></i> --> */}
+                        </ul>       
+                        {/* <!--Create--> */}
+                        <ul><a href="">Create</a></ul>
+                    </div>
+                </ul>
+                {/* <!--icon.symbol--> */}
+                <div class="nav__icons">
+                    <li><i class="fa-solid fa-user"></i></li>
+                        {/* <!-- <li>Profile</li>
+                        <li>Favorites</li>
+                        <li>Watchlist</li>
+                        <li>My Collections</li>
+                        <li>Settings</li>
+                        <li>Language</li>
+                        <ul><a><-Choose Language</a>
+                            <li>English</li>
+                            <li>Spanish</li>
+                            <li>Deutsch</li>
+                            <li>French</li>
+                            <li>Japanese</li>
+                            <li>Korean</li>
+                            <li>Chinese</li>
+                        </ul>
+                        <li>Night Mode.onOff</li> --> */}
+                
+                    {/* <!--Wallet.symbol--> */}
+                    <li><i class="fa-solid fa-wallet"></i></li>
+                    {/* <!--Cart.symbol--> */}
+                    <li><i class="fa-solid fa-cart-shopping"></i></li>
+                </div>
+            </div>
+    </div>
+  );
+}
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './header';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Header></Header>
   </React.StrictMode>,
   document.getElementById('root')
 );
