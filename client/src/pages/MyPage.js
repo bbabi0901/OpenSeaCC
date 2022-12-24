4e657c0a969f78c49e9d5144d540c6ce8b65409c
Russ <yunsubin481@gmail.com>
2022-12-24 20:31:36 +0900

    Create MyPage.js

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,5 +1,13 @@
 const MyPage = () => {
-  return <div>MyPage 미구현임</div>;
+  return <div className="mypage">
+      <div className="background_image"></div>
+      <div className="user_image"></div>
+      <div className="user_info"></div>
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+    </div>;
 };
 
 export default MyPage;
