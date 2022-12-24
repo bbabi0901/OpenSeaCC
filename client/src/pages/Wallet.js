9efee04da2a166387d749b4f5edd15eb84be29de
bbabi0901 <bbabi0901@gmail.com>
2022-12-24 15:35:32 +0900

    metamask

--- client/src/pages/Wallet.js
+++ client/src/pages/Wallet.js
@@ -1,5 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { getWallet } from "../utils/wallet";
+
 const Wallet = (props) => {
-  return <div>Wallet 미구현</div>;
+  const [accounts, setAccounts] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const connectMetamask = async () => {
+      const acc = await getWallet();
+      console.log(acc);
+      setAccounts(acc);
+      if (accounts === undefined) {
+        alert("install metamask extension!!");
+        navigate(-1);
+      }
+    };
+    connectMetamask();
+  });
+
+  return <div>Wallet 미구현, {accounts}</div>;
 };
 
 export default Wallet;
+
+// isWalletConnected => icon color / body
+// wallet type => kaikas / metamask(=klaytn / ethereum)
+// singletone? token?
