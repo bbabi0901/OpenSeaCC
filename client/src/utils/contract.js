6f870bad1b47a8a4902a296a53d16324be42bdc9
bbabi0901 <bbabi0901@gmail.com>
2022-12-27 21:37:06 +0900

    mint module 추가

--- client/src/utils/contract.js
+++ client/src/utils/contract.js
@@ -1,3 +1,14 @@
-import abi from "./ERC721/testAbi.js";
+import abi from "./ERC721/ERC721ABI.js";
+import { getGoerliWeb } from "./wallet.js";
+// import axios from "axios";
 
-console.log(abi);
+const contractAddr = "0x4197425d40E4fDA55C0c2913bcDB325217A7079a";
+const mint = async (address, tokenURI) => {
+  const web = getGoerliWeb();
+  const nftContract = new web.eth.Contract(abi, contractAddr);
+  const addressChecksum = web.utils.toChecksumAddress(address);
+  const result = await nftContract.methods.mintNFT(addressChecksum, tokenURI);
+  return result;
+};
+
+export { mint };
