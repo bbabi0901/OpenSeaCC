511f658bee9a37b9c1b9a20ac2dda911260f2964
Russ <yunsubin481@gmail.com>
2022-12-27 16:19:39 +0900

    NFTdetails update

--- client/src/pages/NFTdetails.js
+++ client/src/pages/NFTdetails.js
@@ -18,52 +18,62 @@ const NFTdetails = ({}) => {
       //
     return(
         <div className="nft_details">
-        <div className="nft_name">    
-        <span className='nft_name'>{/*nft_name*/}</span>
-        </div>
-        <div className="image">
-            <img className="photo" src='https://i.guim.co.uk/img/media/ef8492feb3715ed4de705727d9f513c168a8b196/37_0_1125_675/master/1125.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d456a2af571d980d8b2985472c262b31' alt="nft"></img>
-        </div>
-        <div className='owner'>
-            <h4>Owned by</h4>
-            <a href='/'>{/*user*/}</a>
-        </div>
-        <span className="TradeStation--header">
-            Sale ends 2022년 12월 29일 at 오전 8:06 GMT+9
-            <div className="Timer-numbers">02  17  06  24</div>
-            <div className="Timer-string">Days  Hours  Minutes  Seconds</div>
-            </span>
-        <div className="TradeStation--main">
-            <div className="TradeStation--ask-label">Current price</div>
-            <div className="TradeStation--price-container">
-                0.13
-                ETH
-                $158.66
+            <div className='top'>
+                <div className='top_left'>
+                    <img  className="image" src='https://i.guim.co.uk/img/media/ef8492feb3715ed4de705727d9f513c168a8b196/37_0_1125_675/master/1125.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d456a2af571d980d8b2985472c262b31' alt="nft"></img>
+                    <div className='floor_left'>
+                    <div>
+                        <h4>Description</h4>
+                        <div>
+                            <h4>By</h4> 
+                            <a href='/'>
+                                <h4>LaidBackLlamas</h4>
+                            </a>
+                        <h4>This NFT is a Limited Edition Christmas Lady Llama (Official Collectors NFT) /50.</h4>
+                        </div>
+                        <h4>Details</h4>
+                    </div>
+                </div>
+
+                </div>
+                <div className='top_right'>
+
+                    <div className='info'>
+                        <div className='nft_name'>{/*nft_name*/}</div>
+                        <div className='owner'>
+                            <h4>Owned by</h4>
+                            <a href='/'>{/*user*/}</a>
+                        </div>
+                    </div>
+                    <div className='sale_ends'>
+                        <h4>Sale ends 2022년 12월 29일 at 오전 8:06 GMT+9</h4>
+                        <h4>02  17  06  24</h4>
+                        <h4>Days  Hours  Minutes  Seconds</h4>
+                    </div>
+                    <div className='buy_sell'>
+                        <h4>Current price</h4>
+                        <h4>0.13 ETH ${/*this.props.nft_price*/}</h4>
+                        <button className='add_to_cart'><h3>Add to cart</h3></button>
+                        <button className='make_offer'><h3>Make offer</h3></button>
+                    </div>
+                    <div className='price_history'>
+
+                    </div>
+                    <div className='floor_right'>
+                    <div className='listing'>
+                        <button>
+                            Listings
+                        </button>
+                        <h4>Price &nbsp;USD Price &nbsp;Expiration</h4>
+                        <h4>0.13 ETH &nbsp; $158.66 &nbsp;3days</h4>
+                    </div>
+                </div>
+                </div>      
+
+
+
+                  
             </div>
-            <div>
-                <button className='add_to_cart'>Add to cart</button>
-            </div>
-            <div>
-                <button className='make_offer'>Make offer</button>
-            </div>
-        </div>
-        <button className="Header listing-panel" type="button">
-            <span>Listings</span>
-        </button>
-        <div className="Body listing-panel-header">Price &nbsp;USD Price &nbsp;Expiration</div>
-        <div className="Body listing-panel-main">0.13 ETH &nbsp; $158.66 &nbsp;3days</div>
-        <button className="Header Description-panel">
-            <span>Description</span>
-        </button>
-        <div className="Body Description-panel">
-            <span>By LaidBackLlamas</span>
-            <p>This NFT is a Limited Edition Christmas Lady Llama (Official
-            Collectors NFT) /50.</p>
-        </div>
-        <button className="Header Details-panel">
-            <span>Details</span>
-        </button>
-        <div className=""></div>
         </div>
     );
 }
