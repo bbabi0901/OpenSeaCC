a04dc6a9702cc54969adb65b33fdc2235d159c3c
mamem12 <mamem.dev@gmail.com>
2022-12-27 17:33:49 +0900

    nft inser쿼리 작성

--- server/database/Query.js
+++ server/database/Query.js
@@ -1,7 +1,32 @@
 const query = {
     "GET_USER":"SELECT * FROM user WHERE user_address = ?;",
     "INSERT_USR":"INSERT INTO user (user_address, user_nickname, reg_dt) VALUES (?, ?, NOW());",
-    "GET_NFT" : "SELECT * FROM nft WHERE user_address = ?;"
+    "GET_NFT" : "SELECT * FROM nft WHERE user_address = ?;",
+    "INSERT_NFT" : `
+        INSERT INTO nft 
+        (
+            user_address, 
+            nft_address,
+            nft_name,
+            nft_detail,
+            nft_image,
+            nft_price,
+            reg_dt,
+            upt_dt
+        )
+        VALUES
+        (
+            ?,
+            ?,
+            ?,
+            ?,
+            ?,
+            ?,
+            NOW(),
+            NOW()
+        );
+
+    `
 };
 
 module.exports = query;
