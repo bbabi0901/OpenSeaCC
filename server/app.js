79d322ea916eccc1bf6ebc692700f11ec8503c3a
mamem12 <mamem.dev@gmail.com>
2022-12-27 13:57:24 +0900

    image api 구현

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var mypageRouter = require("./routes/mypage");
 var mintRouter = require("./routes/mint");
 var walletRouter = require("./routes/wallet");
+var imageRouter = require("./routes/image")
 
 const { send } = require('process');
 const { use } = require('./routes/index');
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use("/mypage", mypageRouter);
 app.use("/mint", mintRouter);
 app.use("/wallet",walletRouter);
+app.use("/images", imageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
