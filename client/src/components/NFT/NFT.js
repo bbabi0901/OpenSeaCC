faf8c7bf8cb8659896059ff9f674fc90774a78e5
Russ <yunsubin481@gmail.com>
2022-12-28 13:17:59 +0900

    NFT.js update

--- client/src/components/NFT/NFT.js
+++ client/src/components/NFT/NFT.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './NFT.css'
+import axios from "axios";
+import { useState } from "react";
+
 export default function NFT({nft, handleClick}) {
+  const req = async () => {
+    const result = await axios.post(
+      "http://localhost:3000/nftdetails",
+      isNft, // body
+      { "Content-Type": "application/json" } // header
+    );
+    return result;
+  };
+  const result = req().then((result) => {
+    return result;
+  });
+
+  const [isNft, setIsNft] = useState({
+    user_address: "0x0000000000000000000000000000000000000000",
+    nft_id:"1",
+    nft_address: "test nft address",
+    nft_name: "test nft name",
+    nft_detail: "test nft detail",
+    nft_price: "0.99 ETH $",
+    reg_dt: "",
+    upt_dt: "",
+  });
+  console.log(isNft.nft_id)
     return(
       
-        <Link className="NFT" id='1' key = '1' to="../nftdetails/?id=1">
+        <Link className="NFT" id={isNft.nft_id} key ={isNft.nft_id} to={"../nftdetails/?id="+isNft.nft_id}>
           <div className="nft__content">
             <div className="nft__Info">
               <div className="nft__Info--wrapper">
                 <div className="nft__nft_image"onClick={(e) => handleClick(e, nft.id)}><img src='https://turbofuture.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_xy_center%2Cq_auto:eco%2Cw_1200%2Cx_300%2Cy_489/MTkxODE4MTg0MDIwNjAwMzA2/shutterstock_1928691428.jpg'/></div>
-                이미지
-                nft_id
-                nft_이름
-                nft_price
-                <div className="nft__user_address">asdfsadf</div>
-                <div className="nft__nft_address">1213123123</div>
-                <div className="nft__nft_name">Russ</div>
-                <div className="nft__nft_detail">big, cool</div>
-                <div className="nft__nft_price">$123,430</div>
-                <div className = "nft__reg_dt">1234</div>
-                <div className = "nft__upt_dt">{1234}</div>
+                <div className="nft__nft_address">{isNft.nft_address}</div>
+                <div className="nft__nft_name">{isNft.nft_name}</div>
+                <div className="nft__nft_price">${isNft.nft_price}</div>
               </div>
             </div>
             <div className = "nft__discription"></div>
