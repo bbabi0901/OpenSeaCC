b665c6e588b8137c5de5ce6903142f23dedba984
mamem12 <mamem.dev@gmail.com>
2022-12-28 21:01:33 +0900

    market 백엔드 배포

--- server/controller/marketController.js
+++ server/controller/marketController.js
@@ -5,17 +5,48 @@ const path = require("path")
 module.exports = {
     market : async (req, res) => {
         const category = req.query.category;
+        console.log(category)
 
         if (category == undefined || category == null) {
-            res.sendFile(path.join(__dirname + "../../../client/build/index.html"));
+            return res.sendFile(path.join(__dirname + "../../../client/build/index.html"));
         }
         const result = await execute(query.SELECT_CATEGORY, category);
 
         if (result) {
-            res.send({data:result[0]})
+            return res.send({data:result[0]})
         } else {
-            res.send({data:"fail"});
-
+            return res.send({data:"fail"});
+        }
+    },
+    market_trending : async(req, res) => {
+        const result = await execute(query.SELECT_CATEGORY, "trending");
+        console.log(result[0])
+        if (result) {
+            return res.send({data:result[0]})
+        } else {
+            return res.sendFile(path.join(__dirname + "../../../client/build/index.html"));
+        }
+    },
+    market_art : async(req, res) => {
+        const result = await execute(query.SELECT_CATEGORY, "art");
+        console.log(result[0])
+        if (result) {
+            return res.send({data:result[0]})
+        } else {
+            return res.sendFile(path.join(__dirname + "../../../client/build/index.html"));
+        }
+    },
+    market_collectibles : async(req, res) => {
+        const result = await execute(query.SELECT_CATEGORY, "collectibles");
+        console.log(result[0])
+        if (result) {
+            return res.send({data:result[0]})
+        } else {
+            return res.sendFile(path.join(__dirname + "../../../client/build/index.html"));
         }
     }
 }
+
+// trending
+// art
+// collectibles
--- server/routes/market.js
+++ server/routes/market.js
@@ -4,5 +4,9 @@ var router = express.Router();
 var {market} = require("../controller/marketController")
 
 router.get('/', market);
+router.get('/trending', market);
+router.get('/art', market);
+router.get('/collectibles', market);
+
 
 module.exports = router;
